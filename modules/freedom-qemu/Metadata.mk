bffa06fe5653a9cbc5f2ff7083bc907b0fd183e1
Yunhao Zhang <yz2327@cornell.edu>
2023-09-13 15:05:19 -0400

    Cleanup

--- modules/freedom-qemu/Metadata.mk
+++ modules/freedom-qemu/Metadata.mk
@@ -1,6 +1,6 @@
 # Git repos, branches, commits and folders to get source code for the tool we are building
-RISCV_QEMU_GITURL := git@github.com:sifive/qemu.git
-RISCV_QEMU_BRANCH := v5.2.0-rvv-rvb-zfh
+RISCV_QEMU_GITURL := git@github.com:yhzhang0128/qemu.git
+RISCV_QEMU_BRANCH := egos-qemu-sifive
 RISCV_QEMU_COMMIT := 3870dd3ef58a5c74673388785ad3bfa655af23a4
 RISCV_QEMU_FOLDER := riscv-qemu
 
@@ -8,7 +8,7 @@ RISCV_QEMU_FOLDER := riscv-qemu
 RISCV_QEMU_VERSION := 5.2.0
 
 # Customization ID, which should identify the customization added to the original by SiFive
-FREEDOM_QEMU_ID := 2020.12.0-preview1
+FREEDOM_QEMU_ID := egos-2023.9.8
 
 # Characteristic tags, which should be usable for matching up providers and consumers
 FREEDOM_QEMU_RISCV_TAGS = rv32i rv64i m a f d c v zfh zba zbb
